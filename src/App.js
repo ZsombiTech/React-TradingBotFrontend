14992defc78d7843b8ef62dcea95d3c1ef6c75d4
ZsombiTech <horvathzsombor2005@gmail.com>
2021-09-30 18:44:18 +0200

    background kesz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import Home from "./components/Home";
+import moon from "./Assets/moon.png";
 function App() {
   return (
     <div className="App">
-      <h1>Hello </h1>
+      <img className="moon1" src={moon} alt="Moon picture" />
+      <img className="moon2" src={moon} alt="Moon picture" />
+      <Home />
     </div>
   );
 }
