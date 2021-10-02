64842cca87f7d673dd97eea0c7dde71b73db49ab
ZsombiTech <horvathzsombor2005@gmail.com>
2021-10-02 10:35:59 +0200

    working on the project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,26 @@
 import React, { Fragment } from "react";
 import "../Styling/Navbar.css";
+import house from "../Assets/house.png";
 
 export default function Navbar() {
   return (
     <Fragment>
       <div className="navbar">
-        <div className="homeiconbox">csa</div>
-        <div className="fileiconbox">csa</div>
-        <div className="runningiconbox">csa</div>
-        <div className="bagiconbox">csa</div>
-        <div className="settingsiconbox">csa</div>
+        <div className="homeiconbox">
+          <img className="homeicon" src={house} alt="Home" />
+        </div>
+        <div className="fileiconbox">
+          <img className="homeicon" src={house} alt="Home" />
+        </div>
+        <div className="runningiconbox">
+          <img className="homeicon" src={house} alt="Home" />
+        </div>
+        <div className="bagiconbox">
+          <img className="homeicon" src={house} alt="Home" />
+        </div>
+        <div className="settingsiconbox">
+          <img className="homeicon" src={house} alt="Home" />
+        </div>
       </div>
     </Fragment>
   );
